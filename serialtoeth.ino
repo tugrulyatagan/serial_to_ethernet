76351c00bf4cbd7683a312a907a28d11bc0e5782
Tuğrul Yatağan <tugrulyatagan@gmail.com>
2014-10-15 22:05:08 +0300

    startup dns lookup added

--- serialtoeth.ino
+++ serialtoeth.ino
@@ -1,4 +1,8 @@
+#include <Dns.h>
+
 // cs pini D8 14e kütüphane icinde alindi
+// Enc28J60Network_H icinde
+// #define ENC28J60_CONTROL_CS     8
 
 #include <UIPEthernet.h>
 #include <avr/wdt.h>
@@ -7,25 +11,36 @@ EthernetClient client;
 signed long next;
 String serialInput = "";
 char serialCharacter;
+IPAddress remote_addr;
 
 void setup() {
   Serial.begin(9600);
-  serialInput.reserve(30);
+  serialInput.reserve(50);
 
   wdt_enable(WDTO_8S);
 
-  uint8_t mac[6] = { 0x00,0x01,0x02,0x03,0x04,0x05 };
+  uint8_t mac[6] = { 
+    0x00,0x01,0x02,0x03,0x04,0x05     };
   Ethernet.begin(mac);
 
-  Serial.print("\nlocalIP: ");
-  Serial.println(Ethernet.localIP());
-  //Serial.print("subnetMask: ");
-  //Serial.println(Ethernet.subnetMask());
-  //Serial.print("gatewayIP: ");
-  //Serial.println(Ethernet.gatewayIP());
-  //Serial.print("dnsServerIP: ");
-  //Serial.println(Ethernet.dnsServerIP());
-  //Serial.println();
+//  Serial.print("\nlocalIP: ");
+//  Serial.println(Ethernet.localIP());
+//  Serial.print("subnetMask: ");
+//  Serial.println(Ethernet.subnetMask());
+//  Serial.print("gatewayIP: ");
+//  Serial.println(Ethernet.gatewayIP());
+//  Serial.print("dnsServerIP: ");
+//  Serial.println(Ethernet.dnsServerIP());
+//  Serial.println();
+
+
+  DNSClient dns;
+  dns.begin(Ethernet.dnsServerIP());
+  dns.getHostByName("ituieeelab.org", remote_addr);
+
+//  Serial.println("remote ip:");
+//  Serial.print(remote_addr);
+
   next = 0;
 }
 
@@ -44,8 +59,10 @@ void loop() {
     } 
   }
 
-  next = millis() + 5000; // timeout
-  if (client.connect(IPAddress(192,168,1,100),6000)){
+  next = millis() + 6000; // timeout
+
+  //if (client.connect("ituieeelab.org",6000)){
+  if (client.connect(remote_addr,6000)){
     //Client connected
     //Serial.println(serialInput);
     client.println(serialInput);
@@ -70,4 +87,5 @@ close:
   else
     Serial.println("Client connect failed\n");
 }
+
 
